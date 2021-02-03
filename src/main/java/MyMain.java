addee8bace3bfbd5ada2f3aa65da8fbe49a882b8
AaravSrivastav <22asrivastav@athenian.org>
2021-02-03 04:21:17 +0000

    did the hw

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,11 @@ public class MyMain {
     // in x, recursively
     public static int addDigits(int x) { 
         // YOUR CODE HERE
-        return -1;
+       if(x==0)
+       {
+           return 0;
+       }
+       return x%10 + addDigits(x/10);
     }
 
 
@@ -11,6 +15,17 @@ public class MyMain {
     // (e.g. "racecar", "madam"), recursively
     public static boolean isPalindrome(String str) { 
         // YOUR CODE HERE
+        if(str.length()==0)
+        {
+            return true;
+        }
+        if(str.length()==1)
+        {
+            return true;
+        }
+        if (str.charAt(0) == str.charAt(str.length() - 1)){
+            return (isPalindrome(str.substring(1, str.length() -1)));
+        }
         return false;
     }
 
@@ -18,7 +33,15 @@ public class MyMain {
     // this method should be written using recursion
     public static String reverse(String str) { 
         // YOUR CODE HERE
-        return "";
+        if(str.length()==0)
+        {
+            return str;
+        }
+        if(str.length()==1)
+        {
+            return str;
+        }
+        return str.charAt(str.length()-1)+reverse(str.substring(0, str.length() -1));
     }
 
     public static void main(String[] args) {
